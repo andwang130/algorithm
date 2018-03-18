56698985636a0044a811df3de8f7f45ac7ea73ea
andwang130 <627656470@qq.com>
2018-03-18 23:27:48 +0800

    add 快速排序

--- 快速排序.py
+++ 快速排序.py
@@ -1,5 +1,10 @@
-def sort_(a):
-    pass
-
-
-a=[1,5,6,2,3,54,34,1,23]
+def qosort(li):#快速排序
+   if not li:
+      return li
+   pivot=li[0]
+   lenr=qosort([i for i in li[1:] if i>pivot])
+   genr=qosort([i for i in li[1:] if i<=pivot])
+   return  lenr+[pivot]+genr
+   
+  #快速排序，使用递归的方式，先取下标为0的元素，然后将所有的元素和他对比。大于他的放到一个列表，小于他的放到一个列表
+  #然后将这两个列表在执行这个方法，最后将这些列表。想加起来，组成一个从大到小，或者从小到的列表
